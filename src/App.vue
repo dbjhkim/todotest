4ef5e91f73dcfe2e6b8bc8c564aa4a0a0a715ed5
jaeho <dbjhkim@gmail.com>
2017-07-08 11:04:37 +0900

    Init vue and add basic input form

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="app">
+  <input v-model="newTodo" v-on:keyup.enter="addTodo">
+  <button v-on:click="addTodo">+</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      newTodo: 'Initial Todo Message'
+    }
+  },
+  methods: {
+    addTodo: function() {
+      var value = this.newTodo && this.newTodo.trim()
+      if (value) {
+        console.log('new todo: ' + value)
+      } else {
+        console.log('no new todo')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
